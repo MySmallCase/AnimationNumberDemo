044822a8207b5ad404285d732ea38b6fe06b23b0
向云飞 <978771447@qq.com>
2016-06-12 09:26:08 +0800

    初始化项目

---
+++ animationNumber/animationNumber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animationNumber
+//
+//  Created by 向云飞 on 16/6/12.
+//  Copyright © 2016年 向云飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ animationNumber/animationNumber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animationNumber
+//
+//  Created by 向云飞 on 16/6/12.
+//  Copyright © 2016年 向云飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
